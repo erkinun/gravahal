44b28d43e9a135fa13bac2a9a1e86e93c2df7b38
Erkin Unlu <erkin.unlu@monitise.com>
2014-10-31 17:11:00 +0200

    first controller

---
+++ src/main/java/game/GameController.java
@@ -0,0 +1,18 @@
+package game;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by unlue on 31/10/14.
+ */
+
+@RestController
+public class GameController {
+
+
+    @RequestMapping("/")
+    public String index(){
+        return "hello gravahal game";
+    }
+}
